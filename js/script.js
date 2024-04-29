9404f6eb8ee73094607137beb707910ec0ec5ee3
Tomáš Dvořák <150935816+Dvorinka@users.noreply.github.com>
2024-04-29 21:41:44 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,20 +1,35 @@
+var activeTag = null;
+
 function filterPosts(tag, button) {
-  var posts = document.querySelectorAll('#articleList .post');
-  posts.forEach(function(post) {
-      var tags = post.getAttribute('data-tags').split(' ');
-      if (tags.includes(tag)) {
-          post.style.display = 'block';
-      } else {
-          post.style.display = 'none';
-      }
-  });
+    var posts = document.querySelectorAll('#articleList .post');
+    if (tag === activeTag) {
+        // If the same tag is clicked twice, show all posts
+        posts.forEach(function(post) {
+            post.style.display = 'block';
+        });
+        activeTag = null; // Reset active tag
+        button.classList.remove('active'); // Remove 'active' class from the button
+    } else {
+        // Filter posts by the selected tag
+        posts.forEach(function(post) {
+            var tags = post.getAttribute('data-tags').split(' ');
+            if (tags.includes(tag)) {
+                post.style.display = 'block';
+            } else {
+                post.style.display = 'none';
+            }
+        });
+        activeTag = tag; // Set active tag
 
-  // Remove 'active' class from all buttons
-  var buttons = document.querySelectorAll('.filters button');
-  buttons.forEach(function(btn) {
-      btn.classList.remove('active');
-  });
+        // Remove 'active' class from all buttons except the clicked one
+        var buttons = document.querySelectorAll('.filters button');
+        buttons.forEach(function(btn) {
+            if (btn !== button) {
+                btn.classList.remove('active');
+            }
+        });
 
-  // Add 'active' class to the clicked button
-  button.classList.add('active');
+        // Add 'active' class to the clicked button
+        button.classList.add('active');
+    }
 }
